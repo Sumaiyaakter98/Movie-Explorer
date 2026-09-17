59f16dd91aff1d1312a8a1ed6b877b3cf3cf77ef
Sumaiyaakter98 <sa9075@gmail.com>
2026-09-17 23:56:50 +0600

    Fixed2

--- src/main.jsx
+++ src/main.jsx
@@ -7,35 +7,35 @@ import { createBrowserRouter, RouterProvider } from "react-router";
 import Layout from "./Layout/Layout";
 import MoviListingPage from "./pages/MoviListingPage";
 import MovieDetails from "./pages/MovieDetails";
-import Home from "./Pages/Home";
+import Home from "./pages/Home";
 
 let router = createBrowserRouter([
   {
     path: "/",
     Component: Layout,
     children: [
-      { index: true, 
-        Component: Home 
-      },
-      { path: "/movies",
-        Component: MoviListingPage ,
-        loader:async()=>{
-          const res=await fetch("https://api.tvmaze.com/shows")
-          if(!res.ok){
-            throw new Error("failed")
+      { index: true, Component: Home },
+      {
+        path: "/movies",
+        Component: MoviListingPage,
+        loader: async () => {
+          const res = await fetch("https://api.tvmaze.com/shows");
+          if (!res.ok) {
+            throw new Error("failed");
           }
-          return res.json()
-        }
+          return res.json();
+        },
       },
-      { path: "/movies/:id",
-        Component: MovieDetails ,
-        loader:async({params})=>{
-          const res=await fetch(`https://api.tvmaze.com/shows/${params.id}`)
-          if(!res.ok){
-            throw new Error("failed")
+      {
+        path: "/movies/:id",
+        Component: MovieDetails,
+        loader: async ({ params }) => {
+          const res = await fetch(`https://api.tvmaze.com/shows/${params.id}`);
+          if (!res.ok) {
+            throw new Error("failed");
           }
-          return res.json()
-        }
+          return res.json();
+        },
       },
     ],
   },
